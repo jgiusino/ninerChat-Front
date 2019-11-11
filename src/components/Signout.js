7528be1a6d06de6ce1aea53f32519a61d1afa4ea
Joseph <BlackledgeJoe@protonmail.com>
2019-11-11 18:40:13 -0500

    edited signout page

--- src/components/Signout.js
+++ src/components/Signout.js
@@ -7,17 +7,25 @@ import {
 } from "react-native"
 import { getToken, deleteToken } from './Storage'
 
+
+//this class is called to remove the bearer token and redirects to login page 
+// after verifying that the token is empty
+
 export default class SignOut extends Component {
     constructor(props) {
         super(props);
         this.removeToken();
-    }
+    };
 
     removeToken() {
         let token = getToken();
         token.deleteToken();
-        if(token == 'none'){
-        this.props.navigation.navigate('Login');
+        //print token to the console screen
+        console.log("Token:" + token);
+        if (token == 'none') {
+            this.props.navigation.navigate('Login');
+        } else {
+            console.log("There was an error deleting the token"); //replace this with actual error handling later
         }
-    }
+    };
 }
