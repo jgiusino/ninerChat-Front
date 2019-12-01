7ab4538cee0a215421c2645e1b6de688216f073b
Joseph Blackledge <BlackledgeJoe@protonmail.com>
2019-12-01 17:46:41 -0500

    Added settings to settings page

--- src/Screens/Settings.js
+++ src/Screens/Settings.js
@@ -2,23 +2,29 @@ import React from "react";
 import { getToken } from "../components/Storage";
 import Axios from "axios";
 import {
-    View,
-    Text,
-    StyleSheet,
-    SectionList,
-    TouchableOpacity,
-    Image,
-    Button,
-    TouchEvent,
-    Alert
-  } from "react-native";
-  import Hamburger from "../components/Hamburger";
+  View,
+  Text,
+  StyleSheet,
+  SectionList,
+  TouchableOpacity,
+  Image,
+  Button,
+  TouchEvent,
+  Alert
+} from "react-native";
+import Hamburger from "../components/Hamburger";
+import { FlatList } from "react-native-gesture-handler";
 
 export default class Settings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      token: ""
+      token: "",
+      username: "",
+      email: "",
+      college: "",
+      major: "",
+      admin: ""
     };
   }
 
@@ -29,7 +35,7 @@ export default class Settings extends React.Component {
   };
 
   _fetchProfile = () => {
-    let url = global.URL + "api/profile";
+    let url = global.URL + "/api/profile";
     let collection = {
       token: this.state.token
     };
@@ -40,30 +46,46 @@ export default class Settings extends React.Component {
       data: collection
     })
       .then(response => {
-          let r = response.data;
-          console.log(r);
+        let r = response.data;
+        console.log(r.name);
+        console.log(r.email);
+        console.log(r.admin);
+        console.log(r.major); //major is not being stored on signup
+        console.log(r.college); //college is not being stored on signup
+        this.setState({username: r.name});
+        this.setState({email: r.email});
+        this.setState({admin: r.admin});
+        this.setState({major: r.major});
+        this.setState({college: r.college});
+
       })
       .catch(error => {
         console.log(error);
       });
   };
+
+  componentDidMount() {
+    this._navLister = this.props.navigation.addListener("didFocus", () => {
+      this._fetchToken().then(() => {
+        this._fetchProfile();
+      });
+    });
+  }
   render() {
- 
     return (
       <View style={styles.container}>
         <Hamburger navigation={this.props.navigation} />
 
-       
       </View>
     );
   }
 }
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "#1A1A1B",
-      paddingTop: 10
-    }
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#1A1A1B",
+    paddingTop: 10
+  },
+  
+});
