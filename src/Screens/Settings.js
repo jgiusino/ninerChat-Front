1cd90533d7b1887970a845940c3134957e25bc61
Joseph Blackledge <BlackledgeJoe@protonmail.com>
2019-11-30 23:22:21 -0500

    Fixed settings page/post to server

--- src/Screens/Settings.js
+++ src/Screens/Settings.js
@@ -1,8 +1,69 @@
-import React from 'react';
-import{
-    AsyncStorage
-}from 'react-native'
+import React from "react";
+import { getToken } from "../components/Storage";
+import Axios from "axios";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SectionList,
+    TouchableOpacity,
+    Image,
+    Button,
+    TouchEvent,
+    Alert
+  } from "react-native";
+  import Hamburger from "../components/Hamburger";
 
-export default class Settings extends React.Component{
-    
+export default class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      token: ""
+    };
+  }
+
+  _fetchToken = async () => {
+    let t = await getToken();
+    this.setState({ token: t });
+    console.log("Settings Token: " + this.state.token);
+  };
+
+  _fetchProfile = () => {
+    let url = global.URL + "api/profile";
+    let collection = {
+      token: this.state.token
+    };
+    console.log(JSON.stringify(collection));
+    Axios({
+      method: "post",
+      url: url,
+      data: collection
+    })
+      .then(response => {
+          let r = response.data;
+          console.log(r);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+  render() {
+ 
+    return (
+      <View style={styles.container}>
+        <Hamburger navigation={this.props.navigation} />
+
+       
+      </View>
+    );
+  }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#1A1A1B",
+      paddingTop: 10
+    }
+  });
